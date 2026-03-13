c306824271a4ad7b933bf1bd78cacf8d7c5cedc1
raim <rohakassimbekov@gmail.com>
2026-03-13 20:31:49 +0500

    added 2nd part

---
+++ part2-variables/myscript.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+myname="Raiymbek"
+myage="18"
+mycity="Shymkent"
+
+word=love
+
+##1echo "Good morning! My name is $myname"
+##echo "I'm $myage years old"
+##echo "I was born in $mycity"
+
+##2echo "I $word playing basketball"
+##echo "I $word reading books about psycology"
+##echo "I $word programming and $word money"
+
+
+##3 We can save the output of the command by using subhsell !! variable=$(command)
+
+now=$(date)
+
+##echo "the date right now is:"
+##echo $now
+
+
+
+
