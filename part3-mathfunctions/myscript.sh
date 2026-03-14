10bcd4ddafd25a8040392d91655366e720138ef0
raim <rohakassimbekov@gmail.com>
2026-03-14 17:49:33 +0500

    added 3rd part

---
+++ part3-mathfunctions/myscript.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+## to add , subtract , devide or multiple some numbers we need to write to the at the start !!! expr
+
+mynum1=100
+mynum2=3300
+
+expr $mynum1 + $mynum2
+expr $mynum1 - $mynum2
+expr $mynum1 / $mynum2
+expr $mynum1 \* $mynum2
